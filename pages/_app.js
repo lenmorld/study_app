0a41bea1030666e4e9f83513ae82e365942077e1
Lenmor Larroza Dimanalata <lenmorld@gmail.com>
2021-07-16 07:46:17 -0400

    initialize Redux store - dummy reducers - create store - sample store.dispatch

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import React from "react"
+import "../styles/globals.css"
+
+import store from "../store"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
 export default MyApp
+
+store.dispatch({ type: "questions/add" })
+console.log(store.getState())
