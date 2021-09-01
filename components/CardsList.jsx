f408ee67c2a7ef4b4f0c3ca5021ad813a9fa0017
Lenmor Larroza Dimanalata <lenmorld@gmail.com>
2021-09-01 19:15:06 -0400

    questions selector

--- components/CardsList.jsx
+++ components/CardsList.jsx
@@ -1,5 +1,42 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import { useSelector, useDispatch } from "react-redux"
+
+import store from "../store"
+
+const selectQuestions = (state) => state.questions
+
+const fetchQuestions = async () => {
+  // const raw = await fetch("https://jsonplaceholder.typicode.com/posts")
+  // const posts = await raw.json()
+  // const questions = posts.slice(0, 4).map((post) => ({
+  //   ...post,
+  //   question: post.title,
+  //   answer: post.title,
+  //   type: "open",
+  // }))
+  // store.dispatch({ type: "questions/load", payload: questions })
+}
 
 export default function CardsList(params) {
-  return <h1>CardsList</h1>
+  // instead of accessing store directly, use a selector
+  // const { questions } = store.getState()
+
+  useEffect(() => {
+    fetchQuestions()
+  }, [])
+
+  const questions = useSelector(selectQuestions)
+
+  return (
+    <div>
+      <h1>CardsList</h1>
+      <ul>
+        {questions.map((question) => (
+          <li key={question.id}>
+            {question.question} : {question.answer}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
