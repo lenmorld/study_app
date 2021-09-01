ea21f821d04f8209ca76f255de334f74be46fe5d
Lenmor Larroza Dimanalata <lenmorld@gmail.com>
2021-09-01 19:12:24 -0400

    add redux middleware and enhancers                                      - redux dev tools compose                                      - localStorage: get on load                                      - some utils

--- store.js
+++ store.js
@@ -1,6 +1,30 @@
-import { createStore } from "redux"
+import { createStore, applyMiddleware } from "redux"
+import { composeWithDevTools } from "redux-devtools-extension"
+import thunkMiddleware from "redux-thunk"
+
+import { loadState, saveState } from "./utils/localStorage"
+
 import rootReducer from "./reducers/reducer"
 
-const store = createStore(rootReducer)
+// enhancer
+export const print1 = (storeAPI) => (next) => (action) => {
+  console.log("1")
+  return next(action)
+}
+
+// const middlewareEnhancer = applyMiddleware(print1)
+
+const composedEnhancer = composeWithDevTools(
+  applyMiddleware(print1, thunkMiddleware),
+)
+
+const preloadedState = loadState("quizApp")
+
+const store = createStore(rootReducer, preloadedState, composedEnhancer)
+
+// every state change, save to localStorage
+store.subscribe(() => {
+  saveState("quizApp", store.getState())
+})
 
 export default store
