d6f29fe4e47456af2d3975ca623c52bd59d8bd6d
Lenmor Larroza Dimanalata <lenmorld@gmail.com>
2021-11-18 07:55:34 -0500

    clear alerts after successful login
    - Auth wrapper: add router events handleRouteChange
    - handleRouteChange dispatch clear alert

--- components/Auth.jsx
+++ components/Auth.jsx
@@ -1,9 +1,42 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
+import { useDispatch } from "react-redux"
 import { useRouter } from "next/router"
 import { isUserLoggedIn } from "../utils/auth"
+import { clearAlert } from "../actions/alerts"
 
 export default function Auth({ children }) {
   const router = useRouter()
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const handleRouteChange = (url, { shallow }) => {
+      console.log(
+        `App is changing to ${url} ${
+          shallow ? "with" : "without"
+        } shallow routing`,
+      )
+
+      // clear alerts
+      // * use hooks dispatch
+      // instead of mapDispatchToProps
+      console.log("[ALERT] clear alerts")
+      dispatch(clearAlert())
+    }
+
+    const handleRouterComplete = () => {
+      console.log("complete")
+    }
+
+    router.events.on("routeChangeStart", handleRouteChange)
+    router.events.on("routeChangeComplete", handleRouterComplete)
+
+    // cleanup: when component unmounted
+    // unsubscribe from event
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange)
+      router.events.off("routeChangeComplete", handleRouterComplete)
+    }
+  }, [])
 
   // if (url.includes("login") && isUserLoggedIn()) {
   if (
