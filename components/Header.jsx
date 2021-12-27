f51ad37cbc9c5808358647e3bb324cdd34a1a577
Lenmor Larroza Dimanalata <lenmorld@gmail.com>
2021-12-27 14:00:16 -0500

    Nav items visibility - hide when !auth

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,16 +3,24 @@ import Link from "next/link"
 
 import { useRouter } from "next/router"
 
+import { connect } from "react-redux"
 import styles from "./Header.module.css"
 
-import { logout } from "../services/auth"
+import { isUserLoggedIn } from "../services/auth"
 
-export default function Header() {
+// TODO: use logout action instead
+import { logout } from "../actions/auth"
+// import { logout } from "../services/auth"
+
+function Header({ logoutAction }) {
   const router = useRouter()
 
   const handleLogout = () => {
-    logout()
-    router.push("/login")
+    const successRedirect = () => router.push("/login")
+
+    // logout()
+    // router.push("/login")
+    logoutAction(successRedirect)
   }
 
   return (
@@ -20,36 +28,55 @@ export default function Header() {
       <h2>Studify</h2>
       <nav className={styles.nav}>
         <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/add">
-              <a>New question</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/list">
-              <a>Cards list</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/quiz">
-              <a>Start quiz</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/login">
-              <a>Login</a>
-            </Link>
-          </li>
-          <li>
-            <a onClick={handleLogout}>Logout</a>
-          </li>
+          {!isUserLoggedIn() && (
+            <>
+              <li>
+                <Link href="/login">
+                  <a>Login</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/signup">
+                  <a>Sign up</a>
+                </Link>
+              </li>
+            </>
+          )}
+          {isUserLoggedIn() && (
+            <>
+              <li>
+                <Link href="/">
+                  <a>Home</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/add">
+                  <a>New question</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/list">
+                  <a>Cards list</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/quiz">
+                  <a>Start quiz</a>
+                </Link>
+              </li>
+              <li>
+                <a onClick={handleLogout}>Logout</a>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
   )
 }
+
+const mapDispatchToProps = {
+  logoutAction: logout,
+}
+
+export default connect(null, mapDispatchToProps)(Header)
